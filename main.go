45263f6011ec762df1d726c2a3ff37d263c8ef00
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-10-08 11:26:28 +0100

    3 different ways to declare variables in Go

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 import "fmt"
 
 func main(){
+	// 3 different ways to declare variables in Go
 	var name = "Willow"
-	age := 2
+	name2 := "Dan"
+	var name3 string = "Charles"
+	// age := 2
+	// isRaining := true
 	fmt.Println(name)
-	fmt.Println(age)
+	fmt.Println(name2)
+	fmt.Println(name3)
+	// fmt.Println(age)
+	// fmt.Println(isRaining)
 }
