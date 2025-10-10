369dd8e4c8e300b1586b441ca274d0e6ea609757
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-10-10 17:07:33 +0100

    multiple variable declaration

--- main.go
+++ main.go
@@ -3,14 +3,28 @@ import "fmt"
 
 func main(){
 	// 3 different ways to declare variables in Go
-	var name = "Willow"
-	name2 := "Dan"
-	var name3 string = "Charles"
+	// var name = "Willow"
+	// name2 := "Dan"
+	// var name3 string = "Charles"
+
+	// Multiple variable declaration
+	var one, two, three, four, five int = 1, 2, 3, 4, 5 
+
+	var(
+		num int
+		number int = 1
+		greetings string = "hello"
+	)
 	// age := 2
 	// isRaining := true
-	fmt.Println(name)
-	fmt.Println(name2)
-	fmt.Println(name3)
+	fmt.Println(one, two, three, four, five)
+	fmt.Println(num)
+	fmt.Println(number)
+	fmt.Println(greetings)
+	
 	// fmt.Println(age)
 	// fmt.Println(isRaining)
+
+	
+	
 }
