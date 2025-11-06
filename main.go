f14c77c9d5b0ddab3151ef84ee846d308db679d6
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-11-06 23:58:33 +0100

    Switch case example 2

--- main.go
+++ main.go
@@ -69,4 +69,24 @@ func main(){
 	for i, day := range days{
 		fmt.Println(i, day)
 	}
+
+	day := 3
+	switch day{
+		case 1:
+			fmt.Println("Monday")
+		case 2:
+			fmt.Println("Tuesday")
+		case 3:
+			fmt.Println("Wednesday")	
+		case 4:
+			fmt.Println("Thursday")
+		case 5:
+			fmt.Println("Friday")
+		case 6:
+			fmt.Println("Saturday")	
+		case 7:
+			fmt.Println("Sunday")
+		default:
+			fmt.Println("Invalid day")
+	}
 }
